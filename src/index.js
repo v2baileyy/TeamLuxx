49015b494e4cf04fb50eb99d5ed8e9916a626ba8
v2baileyy <132116207+v2baileyy@users.noreply.github.com>
2026-03-18 16:46:40 -0500

    Refactor index.js to use React for rendering
    Removed HTML boilerplate and integrated React rendering.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#030014">
-  <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
-  <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-  
-  <!-- Loads Tailwind CSS -->
-  <script src="https://cdn.tailwindcss.com"></script>
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-  <title>Team Luxx Esports</title>
-</head>
-<body>
-  <noscript>You need to enable JavaScript to run this app.</noscript>
-  <div id="root"></div>
-</body>
-</html>
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
